93cbc95b3e8a4c94be5e2f162090d0337708fb66
Alex <avzproject@gmail.com>
2023-04-20 20:43:18 +0300

    FEATURED PRODUCTS tmp

--- wp-content/themes/jewstore/front-page.php
+++ wp-content/themes/jewstore/front-page.php
@@ -1,27 +1,27 @@
 <?php /* Template Name: Front Page */ ?>
 
 <?php get_header(); ?>
-        <section class="front-slider" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/assets/Pic_Slider.png')">
+        <section class="front-slider" style="background-image: url('<?php echo get_stylesheet_directory_uri() . esc_html(get_theme_mod( 'jw_bg_link' )) ?>')">
             <div class="container d-flex align-items-center h-100">
                 <div class="front-slider__text">
                     <?php
-                        if( get_post_meta( get_the_ID(), 'Header_Text_1' ) ) {
-                            echo '<p class="text-1">' . get_post_meta( get_the_ID(), 'Header_Text_1' )[0] . '</p>';
+                        if( esc_html(get_theme_mod( 'jw_header_1' )) != '' ) {
+                            echo '<p class="text-1">' . esc_html(get_theme_mod( 'jw_header_1' )) . 'test</p>';
                         }
                     ?>
                     <?php
-                        if( get_post_meta( get_the_ID(), 'Header_Text_1' ) ) {
-                            echo '<h1 class="text-2">' . get_post_meta( get_the_ID(), 'Header_Text_2' )[0] . '</h1>';
+                        if( esc_html(get_theme_mod( 'jw_header_2' )) ) {
+                            echo '<h1 class="text-2">' . esc_html(get_theme_mod( 'jw_header_2' )) . '</h1>';
                         }
                     ?>
                     <?php
-                        if( get_post_meta( get_the_ID(), 'Header_Text_3' ) ) {
-                            echo '<p class="text-3">' . get_post_meta( get_the_ID(), 'Header_Text_3' )[0] . '</p>';
+                        if( esc_html(get_theme_mod( 'jw_header_3' )) ) {
+                            echo '<p class="text-3">' . esc_html(get_theme_mod( 'jw_header_3' )) . '</p>';
                         }
                     ?>
                     <?php
-                        $button1Link = get_post_meta( get_the_ID(), 'Header_Button_1_link' )[0];
-                        $button1Text = get_post_meta( get_the_ID(), 'Header_Button_1' )[0];
+                        $button1Link = esc_html(get_theme_mod( 'jw_button_1_link' ));
+                        $button1Text = esc_html(get_theme_mod( 'jw_button_1' ));
 
                         if( $button1Text != '' ) {
                     ?>
@@ -32,8 +32,8 @@
                         } ?>"><?= $button1Text ?></a>
                     <?php } ?>
                     <?php
-                        $button2Link = get_post_meta( get_the_ID(), 'Header_Button_2_link' )[0];
-                        $button2Text = get_post_meta( get_the_ID(), 'Header_Button_2' )[0];
+                        $button2Link = esc_html(get_theme_mod( 'jw_button_2_link' ));
+                        $button2Text = esc_html(get_theme_mod( 'jw_button_2' ));
 
                         if( $button2Text != '' ) {
                     ?>
@@ -62,22 +62,46 @@
             ?>
 	    </div>
 	    <div class="block-1__right text-center">
-	        <div class="zhadan-alex-testovoye">
-                Donec sollicitudin
-            </div>
-            <div class="zhadan-alex-testovoye-1">
-                JEWELLERY
-            </div>
-            <div class="zhadan-alex-testovoye-1 zhadan-alex-testovoye-1--small">
-                STORE
-            </div>
-            <div class="zhadan-alex-testovoye-2">
-                An sincerity so extremity he additions. Her yet there truth merit. Mrs all projecting favourable now unpleasing. Son law garden chatty temper. Oh children provided to mr elegance marriage strongly.
-            </div>
+             <div class="zhadan-alex-testovoye">
+                <?php
+                    if( esc_html(get_theme_mod( 'jw_block_1_header_1' )) != '' ) {
+                        echo esc_html(get_theme_mod( 'jw_block_1_header_1' ));
+                    } else {
+                        echo 'Donec sollicitudin';
+                    }
+                ?>
+             </div>
+             <div class="zhadan-alex-testovoye-1">
+                <?php
+                    if( esc_html(get_theme_mod( 'jw_block_1_header_2' )) != '' ) {
+                        echo esc_html(get_theme_mod( 'jw_block_1_header_2' ));
+                    } else {
+                        echo 'JEWELLERY';
+                    }
+                ?>
+             </div>
+             <div class="zhadan-alex-testovoye-1 zhadan-alex-testovoye-1--small">
+                <?php
+                    if( esc_html(get_theme_mod( 'jw_block_1_header_3' )) != '' ) {
+                        echo esc_html(get_theme_mod( 'jw_block_1_header_3' ));
+                    } else {
+                        echo 'STORE';
+                    }
+                ?>
+             </div>
+             <div class="zhadan-alex-testovoye-2">
+                <?php
+                    if( esc_html(get_theme_mod( 'jw_block_1_header_4' )) != '' ) {
+                        echo esc_html(get_theme_mod( 'jw_block_1_header_4' ));
+                    } else {
+                        echo 'An sincerity so extremity he additions. Her yet there truth merit. Mrs all projecting favourable now unpleasing. Son law garden chatty temper. Oh children provided to mr elegance marriage strongly.';
+                    }
+                 ?>
+             </div>
             <div>
                 <?php
-                    $button10Link = get_post_meta( get_the_ID(), 'Block_1_Button_1_link' )[0];
-                    $button10Text = get_post_meta( get_the_ID(), 'Block_1_Button_1' )[0];
+                    $button10Link = esc_html(get_theme_mod( 'jw_block_1_Button_link' ));
+                    $button10Text = esc_html(get_theme_mod( 'jw_block_1_Button' ));
 
                     if( $button10Text != '' ) {
                 ?>
@@ -88,8 +112,8 @@
                     } ?>"><?= $button10Text ?></a>
                 <?php } ?>
                 <?php
-                    $button20Link = get_post_meta( get_the_ID(), 'Block_1_Button_2_link' )[0];
-                    $button20Text = get_post_meta( get_the_ID(), 'Block_1_Button_2' )[0];
+                    $button20Link = esc_html(get_theme_mod( 'jw_block_2_Button_link' ));
+                    $button20Text = esc_html(get_theme_mod( 'jw_block_2_Button' ));
 
                     if( $button20Text != '' ) {
                 ?>
@@ -101,5 +125,142 @@
                 <?php } ?>
             </div>
 	    </div>
+	</section>
+	<section class="block-2 container">
+	    <div class="zhadan-alex-testovoye text-center">
+            <?php
+                if( esc_html(get_theme_mod( 'jw_block_2_header_1' )) != '' ) {
+                    echo esc_html(get_theme_mod( 'jw_block_2_header_1' ));
+                } else {
+                    echo 'Adipisicing elit';
+                }
+            ?>
+        </div>
+        <div class="zhadan-alex-testovoye-3 text-center">
+            <?php
+                if( esc_html(get_theme_mod( 'jw_block_2_header_2' )) != '' ) {
+                    echo esc_html(get_theme_mod( 'jw_block_2_header_2' ));
+                } else {
+                    echo 'FEATURED PRODUCTS';
+                }
+            ?>
+        </div>
+        <div class="zhadan-alex-testovoye-2 text-center mb-50">
+            <?php
+                if( esc_html(get_theme_mod( 'jw_block_2_header_3' )) != '' ) {
+                    echo esc_html(get_theme_mod( 'jw_block_2_header_3' ));
+                } else {
+                    echo 'There are many variations of passages of lorem ipsum available.';
+                }
+            ?>
+        </div>
+        <div class="d-flex mb-30">
+            <div class="block-2__left position-relative">
+                <?php
+                    $imgBannerAlt = get_post_meta( get_the_ID(), 'Banner_1_img_alt' )[0];
+                    $imgBannerSrc = get_post_meta( get_the_ID(), 'Banner_1_img' )[0];
+    
+                    if( $imgBannerSrc != '' ) {
+                        echo '<img src="' . $imgBannerSrc . '" alt="' . $imgBannerAlt . '" width="624px" height="500px" />';
+                    } else {
+                        echo '<img src="' . get_stylesheet_directory_uri() . '/assets/banner-1.png" alt="' . $imgBannerAlt . '" width="270px" height="370px">';
+                    }
+                ?>
+                <div class="banner-text text-center d-flex">
+                    <div>
+                        <div class="banner-text__header-text">
+                            <?php
+                                $Banner1textHeader = get_post_meta( get_the_ID(), 'Banner_1_text_header' )[0];
+
+                                if( $Banner1textHeader != '' ) {
+                                    echo $Banner1textHeader;
+                                } else {
+                                    echo 'TRISTIQUE JUSTO';
+                                }
+                            ?>
+                        </div>
+                        <div class="banner-text__regular-text">
+                            <?php
+                                $Banner1text = get_post_meta( get_the_ID(), 'Banner_1_text' )[0];
+
+                                if( $Banner1text != '' ) {
+                                    echo $Banner1text;
+                                } else {
+                                    echo 'Started now shortly had for assured hearing expense led juvenile.';
+                                }
+                            ?>
+                        </div>
+                        <div class="banner-text__shop-now">
+                            shop now
+                        </div>
+                        <a href="#" class="fake-link"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="block-2__right">
+                <div class="slider">
+                    <?php
+                        $allData = get_field( "featured_products" );
+                        $mas = get_field( "featured_products" )[0];
+
+                        for($i = 0; $i < count($allData); $i++) {
+                            $oneProduct = $allData[$i];
+                            $wc_product = wc_get_product( $oneProduct->ID );
+
+                            echo '<div class="slider__item"><div class="product__name">' . $oneProduct->post_title . '</div>';
+                            echo '<div class="product__name">' . $wc_product->get_regular_price() . '</div>';
+                            echo '<div class="product__name">' . get_the_post_thumbnail($oneProduct->ID) . '</div></div>';
+                        }
+                    ?>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex">
+            <div class="block-2__left position-relative">
+                <?php
+                    $imgBanner2Alt = get_post_meta( get_the_ID(), 'Banner_2_img_alt' )[0];
+                    $imgBanner2Src = get_post_meta( get_the_ID(), 'Banner_2_img' )[0];
+    
+                    if( $imgBanner2Src != '' ) {
+                        echo '<img src="' . $imgBanner2Src . '" alt="' . $imgBanner2Alt . '" width="624px" height="500px" />';
+                    } else {
+                        echo '<img src="' . get_stylesheet_directory_uri() . '/assets/banner-2.png" alt="' . $imgBanner2Alt . '" width="270px" height="370px">';
+                    }
+                ?>
+                <div class="banner-text text-center d-flex">
+                    <div>
+                        <div class="banner-text__header-text white">
+                            <?php
+                                $Banner2textHeader = get_post_meta( get_the_ID(), 'Banner_2_text_header' )[0];
+
+                                if( $Banner2textHeader != '' ) {
+                                    echo $Banner2textHeader;
+                                } else {
+                                    echo 'TRISTIQUE JUSTO';
+                                }
+                            ?>
+                        </div>
+                        <div class="banner-text__regular-text gray">
+                            <?php
+                                $Banner2text = get_post_meta( get_the_ID(), 'Banner_2_text' )[0];
+
+                                if( $Banner2text != '' ) {
+                                    echo $Banner2text;
+                                } else {
+                                    echo 'Started now shortly had for assured hearing expense led juvenile.';
+                                }
+                            ?>
+                        </div>
+                        <div class="banner-text__shop-now white">
+                            shop now
+                        </div>
+                        <a href="#" class="fake-link"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="block-2__right">
+                slider
+            </div>
+        </div>
 	</section>
 <?php get_footer(); ?>
--- wp-content/themes/jewstore/functions.php
+++ wp-content/themes/jewstore/functions.php
@@ -66,3 +66,474 @@ function header_add_to_cart_fragment( $fragments ) {
     $fragments['.basket-btn__counter'] = ob_get_clean();
     return $fragments;
 }
+
+/*
+* Theme options
+*/
+
+add_action( 'customize_register', 'true_customizer_init' );
+
+function true_customizer_init( $wp_customize ) {
+
+	$wp_customize->add_panel( 'true_panel',
+        array(
+            'priority'       => 100,
+            'title'          => 'JewShop Settings by Zhadan',
+            'description'    => 'Here you can customize the appearance of your site.',
+        )
+    );
+
+    $wp_customize->add_section(
+    	'true_header_section',
+    	array(
+    		'title'       => 'Header',
+    		'priority'    => 1,
+    		'description' => 'Here you can customize the header of your site.',
+    		'panel'       => 'true_panel'
+    	)
+    );
+
+    $wp_customize->add_setting(
+    	'jw_header_2',
+    	array(
+    		'default'    =>  'JEWELLERY STORE',
+    		'transport'  =>  'postMessage'
+    	)
+    );
+
+    $wp_customize->add_control(
+    	'jw_header_2',
+    	array(
+    		'section'   => 'true_header_section',
+    		'label'     => 'Header 2',
+    		'type'      => 'text'
+    	)
+    );
+
+    $wp_customize->add_setting(
+    	'jw_header_3',
+    	array(
+    		'default'    =>  'An sincerity so extremity he additions. Her yet there truth merit. Mrs all projecting favourable now unpleasing. Son law garden chatty temper. Oh children provided to mr elegance marriage strongly', // для чекбокса значения по умолчанию true/false
+    		'transport'  =>  'postMessage'
+    	)
+    );
+
+    $wp_customize->add_control(
+    	'jw_header_3',
+    	array(
+    		'section'   => 'true_header_section',
+    		'label'     => 'Header 3',
+    		'type'      => 'text'
+    	)
+    );
+
+    $wp_customize->add_setting(
+    	'jw_button_1',
+    	array(
+    		'default'    =>  'shop now',
+    		'transport'  =>  'postMessage'
+    	)
+    );
+
+    $wp_customize->add_control(
+    	'jw_button_1',
+    	array(
+    		'section'   => 'true_header_section',
+    		'label'     => 'Button 1',
+    		'type'      => 'text'
+    	)
+    );
+
+    $wp_customize->add_setting(
+    	'jw_button_1_link',
+    	array(
+    		'default'    =>  '#',
+    		'transport'  =>  'postMessage'
+    	)
+    );
+
+    $wp_customize->add_control(
+    	'jw_button_1_link',
+    	array(
+    		'section'   => 'true_header_section',
+    		'label'     => 'Button 1 Link',
+    		'type'      => 'text'
+    	)
+    );
+
+    $wp_customize->add_setting(
+    	'jw_button_2',
+    	array(
+    		'default'    =>  'shop now',
+    		'transport'  =>  'postMessage'
+    	)
+    );
+
+    $wp_customize->add_control(
+    	'jw_button_2',
+    	array(
+    		'section'   => 'true_header_section',
+    		'label'     => 'Button 2',
+    		'type'      => 'text'
+    	)
+    );
+
+    $wp_customize->add_setting(
+    	'jw_button_2_link',
+    	array(
+    		'default'    =>  '#',
+    		'transport'  =>  'postMessage'
+    	)
+    );
+
+    $wp_customize->add_control(
+    	'jw_button_2_link',
+    	array(
+    		'section'   => 'true_header_section',
+    		'label'     => 'Button 2 Link',
+    		'type'      => 'text'
+    	)
+    );
+
+    $wp_customize->add_setting(
+    	'jw_bg_link',
+    	array(
+    		'default'    =>  './wp-content/themes/jewstore/assets/Pic_Slider.png',
+    		'transport'  =>  'postMessage'
+    	)
+    );
+
+    $wp_customize->add_control(
+    	'jw_bg_link',
+    	array(
+    		'section'   => 'true_header_section',
+    		'label'     => 'Background img Link',
+    		'type'      => 'text'
+    	)
+    );
+
+    // Block 1
+
+    $wp_customize->add_section(
+        'true_block_1_section',
+        array(
+            'title'       => 'Block 1',
+            'priority'    => 2,
+            'description' => 'Here you can customize the Block 1 of your site.',
+            'panel'       => 'true_panel'
+        )
+    );
+
+    $wp_customize->add_setting(
+        'jw_block_1_header_1',
+        array(
+            'default'    =>  'Donec sollicitudin',
+            'transport'  =>  'postMessage'
+        )
+    );
+
+    $wp_customize->add_control(
+        'jw_block_1_header_1',
+        array(
+            'section'   => 'true_block_1_section',
+            'label'     => 'Header 1',
+            'type'      => 'text'
+        )
+    );
+
+    $wp_customize->add_setting(
+        'jw_block_1_header_2',
+        array(
+            'default'    =>  'JEWELLERY',
+            'transport'  =>  'postMessage'
+        )
+    );
+
+    $wp_customize->add_control(
+        'jw_block_1_header_2',
+        array(
+            'section'   => 'true_block_1_section',
+            'label'     => 'Header 2',
+            'type'      => 'text'
+        )
+    );
+
+    $wp_customize->add_setting(
+        'jw_block_1_header_3',
+        array(
+            'default'    =>  'STORE',
+            'transport'  =>  'postMessage'
+        )
+    );
+
+    $wp_customize->add_control(
+        'jw_block_1_header_3',
+        array(
+            'section'   => 'true_block_1_section',
+            'label'     => 'Header 3',
+            'type'      => 'text'
+        )
+    );
+
+    $wp_customize->add_setting(
+        'jw_block_1_header_4',
+        array(
+            'default'    =>  'An sincerity so extremity he additions. Her yet there truth merit. Mrs all projecting favourable now unpleasing. Son law garden chatty temper. Oh children provided to mr elegance marriage strongly.',
+            'transport'  =>  'postMessage'
+        )
+    );
+
+    $wp_customize->add_control(
+        'jw_block_1_header_4',
+        array(
+            'section'   => 'true_block_1_section',
+            'label'     => 'Text',
+            'type'      => 'text'
+        )
+    );
+
+    $wp_customize->add_setting(
+        'jw_block_1_Button',
+        array(
+            'default'    =>  'shop now',
+            'transport'  =>  'postMessage'
+        )
+    );
+
+    $wp_customize->add_control(
+        'jw_block_1_Button',
+        array(
+            'section'   => 'true_block_1_section',
+            'label'     => 'Button 1 Text',
+            'type'      => 'text'
+        )
+    );
+
+    $wp_customize->add_setting(
+        'jw_block_1_Button_link',
+        array(
+            'default'    =>  '#',
+            'transport'  =>  'postMessage'
+        )
+    );
+
+    $wp_customize->add_control(
+        'jw_block_1_Button_link',
+        array(
+            'section'   => 'true_block_1_section',
+            'label'     => 'Button 1 link',
+            'type'      => 'text'
+        )
+    );
+
+    // Block 2
+    
+    $wp_customize->add_section(
+        'true_block_2_section',
+        array(
+            'title'       => 'FEATURED PRODUCTS',
+            'priority'    => 3,
+            'description' => 'Here you can customize the FEATURED PRODUCTS of your site.',
+            'panel'       => 'true_panel'
+        )
+    );
+
+    $wp_customize->add_setting(
+        'jw_block_2_header_1',
+        array(
+            'default'    =>  'Adipisicing elit',
+            'transport'  =>  'postMessage'
+        )
+    );
+
+    $wp_customize->add_control(
+        'jw_block_2_header_1',
+        array(
+            'section'   => 'true_block_2_section',
+            'label'     => 'Header 1',
+            'type'      => 'text'
+        )
+    );
+
+    $wp_customize->add_setting(
+        'jw_block_2_header_2',
+        array(
+            'default'    =>  'FEATURED PRODUCTS',
+            'transport'  =>  'postMessage'
+        )
+    );
+
+    $wp_customize->add_control(
+        'jw_block_2_header_2',
+        array(
+            'section'   => 'true_block_2_section',
+            'label'     => 'Header 2',
+            'type'      => 'text'
+        )
+    );
+
+    $wp_customize->add_setting(
+        'jw_block_2_header_3',
+        array(
+            'default'    =>  'There are many variations of passages of lorem ipsum available.',
+            'transport'  =>  'postMessage'
+        )
+    );
+
+    $wp_customize->add_control(
+        'jw_block_2_header_3',
+        array(
+            'section'   => 'true_block_2_section',
+            'label'     => 'Text',
+            'type'      => 'text'
+        )
+    );
+
+    $wp_customize->add_setting(
+        'jw_block_2_banner_img',
+        array(
+            'default'    =>  '/assets/banner-1.png',
+            'transport'  =>  'postMessage'
+        )
+    );
+
+    $wp_customize->add_control(
+        'jw_block_2_banner_img',
+        array(
+            'section'   => 'true_block_2_section',
+            'label'     => 'Banner 1 Img Url',
+            'type'      => 'text'
+        )
+    );
+
+    $wp_customize->add_setting(
+        'jw_block_2_banner_header',
+        array(
+            'default'    =>  'TRISTIQUE JUSTO',
+            'transport'  =>  'postMessage'
+        )
+    );
+
+    $wp_customize->add_control(
+        'jw_block_2_banner_header',
+        array(
+            'section'   => 'true_block_2_section',
+            'label'     => 'Banner 1 Header',
+            'type'      => 'text'
+        )
+    );
+
+    $wp_customize->add_setting(
+        'jw_block_2_banner_text',
+        array(
+            'default'    =>  'Started now shortly had for assured hearing expense led juvenile.',
+            'transport'  =>  'postMessage'
+        )
+    );
+
+    $wp_customize->add_control(
+        'jw_block_2_banner_text',
+        array(
+            'section'   => 'true_block_2_section',
+            'label'     => 'Banner 1 Text',
+            'type'      => 'text'
+        )
+    );
+
+    $wp_customize->add_setting(
+        'jw_block_2_banner_link',
+        array(
+            'default'    =>  '#',
+            'transport'  =>  'postMessage'
+        )
+    );
+
+    $wp_customize->add_control(
+        'jw_block_2_banner_link',
+        array(
+            'section'   => 'true_block_2_section',
+            'label'     => 'Banner 1 Link',
+            'type'      => 'text'
+        )
+    );
+
+    $wp_customize->add_setting(
+        'jw_block_2_banner_2_img',
+        array(
+            'default'    =>  '/assets/banner-2.png',
+            'transport'  =>  'postMessage'
+        )
+    );
+
+    $wp_customize->add_control(
+        'jw_block_2_banner_2_img',
+        array(
+            'section'   => 'true_block_2_section',
+            'label'     => 'Banner 2 Img Url',
+            'type'      => 'text'
+        )
+    );
+
+    $wp_customize->add_setting(
+        'jw_block_2_banner_2_header',
+        array(
+            'default'    =>  'TRISTIQUE JUSTO',
+            'transport'  =>  'postMessage'
+        )
+    );
+
+    $wp_customize->add_control(
+        'jw_block_2_banner_2_header',
+        array(
+            'section'   => 'true_block_2_section',
+            'label'     => 'Banner 2 Header',
+            'type'      => 'text'
+        )
+    );
+
+    $wp_customize->add_setting(
+        'jw_block_2_banner_2_text',
+        array(
+            'default'    =>  'Started now shortly had for assured hearing expense led juvenile.',
+            'transport'  =>  'postMessage'
+        )
+    );
+
+    $wp_customize->add_control(
+        'jw_block_2_banner_2_text',
+        array(
+            'section'   => 'true_block_2_section',
+            'label'     => 'Banner 2 Text',
+            'type'      => 'text'
+        )
+    );
+
+    $wp_customize->add_setting(
+        'jw_block_2_banner_2_link',
+        array(
+            'default'    =>  '#',
+            'transport'  =>  'postMessage'
+        )
+    );
+
+    $wp_customize->add_control(
+        'jw_block_2_banner_2_link',
+        array(
+            'section'   => 'true_block_2_section',
+            'label'     => 'Banner 2 Link',
+            'type'      => 'text'
+        )
+    );
+
+    // Footer
+
+    $wp_customize->add_section(
+        'true_footer_section',
+        array(
+            'title'       => 'Footer',
+            'priority'    => 5,
+            'description' => 'Here you can customize the footer of your website.',
+            'panel'       => 'true_panel'
+        )
+    );
+}
